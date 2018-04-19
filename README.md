2ac8bf060b53977a4b4dca097882268933dfea6d
Josef A. Puckl <info@ecomstyle.de>
2018-04-19 18:11:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+FixSmartyInContent
+==================
+
+### Shopversion
+OXID eShop 6
+
+
+### Features
+Verwandelt beim Speichern von CMS-Inhalten ´-&gt;´ in ´->´ .
+Soll angeblich bei der Verwendung von Smarty-Tags in CMS-Inhalten (auch Emails) helfen.
+
+
+### Installation
+`composer require ecs/fixsmartyincontent`
+
+
+### License
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.  
---
+++ metadata.php
@@ -0,0 +1,33 @@
+<?php
+/*    Please retain this copyright header in all versions of the software
+ *
+ *    Copyright (C) Josef A. Puckl | eComStyle.de
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see {http://www.gnu.org/licenses/}.
+ */
+
+$sMetadataVersion = '2.0';
+$aModule = array(
+    'id'           => 'ecs_fixsmartyincontent',
+    'title'        => '<strong style="color:#04B431;">e</strong><strong>ComStyle.de</strong>:  <i>FixSmartyInContent</i>',
+    'description'  => 'Repariert vom Wysiwyg-Editor zerstörte >',
+    'version'      => '1.0.0',
+    'thumbnail'    => 'ecs.png',
+    'author'       => '<strong style="font-size: 17px;color:#04B431;">e</strong><strong style="font-size: 16px;">ComStyle.de</strong>',
+    'email'        => 'info@ecomstyle.de',
+    'url'          => 'https://ecomstyle.de',
+    'extend'       =>  array(
+        \OxidEsales\Eshop\Application\Controller\Admin\ContentMain::class       => Ecs\FixSmartyInContent\Controller\Admin\ContentMain::class,
+    )
+);
